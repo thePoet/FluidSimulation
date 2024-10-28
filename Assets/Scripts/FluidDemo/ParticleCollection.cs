424b2f631e2ffacea2b4d824d261fc4296e406fa
Riku Erkkila <riku.erkkila@gmail.com>
2024-10-28 16:39:37 +0200

    SpannableDictionary
    - Generalized version of ParticleCollection made: SpannableDictionary
    - SpannableDictionary and SpatalPartitioningGrid moved into RikusGameDevToolbox

--- Assets/Scripts/FluidDemo/ParticleCollection.cs
+++ Assets/Scripts/FluidDemo/ParticleCollection.cs
@@ -1,81 +1,12 @@
-using System;
-using System.Collections.Generic;
+using RikusGameDevToolbox.GeneralUse;
 
 namespace FluidDemo
 {
-    public class ParticleCollection
+    public class ParticleCollection : SpannableDictionary<ParticleId, Particle>
     {
-        private Dictionary<ParticleId, int> _IdToIndex;
-        private Dictionary<int, ParticleId> _IndexToId;
-        private Particle[] _array;
-
-        public int Count => _IdToIndex.Count;
+        public ParticleCollection(int maxNumEntries) : base(maxNumEntries) { }
         
-        public ParticleCollection(int maxNumParticles)
-        {
-            _IdToIndex = new Dictionary<ParticleId, int>();
-            _IndexToId = new Dictionary<int, ParticleId>();
-            _array = new Particle[maxNumParticles];
-        }
-
-
-        public Particle Get(ParticleId id)
-        {
-            return _array[_IdToIndex[id]];
-        }
-        
-        public void Add(Particle particle)
-        {
-            int index = _IdToIndex.Count;
-            _array[index] = particle;
-            _IdToIndex.Add(particle.Id, index);
-            _IndexToId.Add(index, particle.Id);
-        }
-
-        public void Remove(ParticleId id)
-        {
-            int index = _IdToIndex[id];
-            int lastIndex = _IdToIndex.Count - 1;
-
-            _IdToIndex.Remove(id);
-            _IndexToId.Remove(index);
-            
-            if (index == lastIndex) return;
-            
-            // Move last element in array to the empty spot so that the array is contiguous:
-            _array[index] = _array[lastIndex];
-
-            //..and adjust the dictionaries accordingly:
-            ParticleId lastElementId = _IndexToId[lastIndex];
-            _IdToIndex[lastElementId] = index;
-            _IndexToId[index] = lastElementId;
-            _IndexToId.Remove(lastIndex);
-        }
-
-        public void Update(Particle particle)
-        {
-            _array[_IdToIndex[particle.Id]] = particle;
-        }
-
-        public void Clear()
-        {
-            _IdToIndex.Clear();
-            _IndexToId.Clear();
-        }
-        
-        public Span<Particle> AsSpan()
-        {
-            return _array.AsSpan().Slice(0, _IdToIndex.Count);
-        }
-        
-        public int SpanIndexOf(ParticleId id)
-        {
-            return _IdToIndex[id];
-        }
-        
-        
-     
-        
-        
+        public void Add(Particle particle) =>  base.Add(particle.Id, particle);
+        public void Update(Particle particle) => base.Update(particle.Id, particle);
     }
 }
--- Assets/Scripts/RikusGameDevToolbox/GeneralUse/SpatialPartitioningGrid.cs
+++ Assets/Scripts/RikusGameDevToolbox/GeneralUse/SpatialPartitioningGrid.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using RikusGameDevToolbox.GeneralUse;
 
-namespace FluidDemo
+namespace RikusGameDevToolbox.GeneralUse
 {
     public class SpatialPartitioningGrid<T>
     {
